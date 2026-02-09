577fb7df61431fd82aeb1f89888b9d8c3cb29e8e
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-09 21:23:13 +0530

    Practice: React src and components basics

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Card from './components/card'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Prince Kushwaha</h1>
+        <p>Software Developer</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
