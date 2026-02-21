db32f35fc45848c6cd611f259e4a192233e55f42
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-02-21 23:19:18 +0530

    add : props base structure

--- 03-card-project/src/App.jsx
+++ 03-card-project/src/App.jsx
@@ -3,7 +3,13 @@ import Card from './assets/components/card'
 const App = () => {
   return (
     <div>
-      <Card/>
+       <Card src="https://www.bing.com/th/id/OIP.CrfHaFNOQMDTJL_aBDVFwQHaHh?w=160&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+      companyName="Google" jobTitle="Frontend Developer" jobType="Part Time" level="Mid Level" salary="$80" location="Bangalore, India"
+      />
+      <Card src="https://media.wired.com/photos/5a99ba72927dc94e67685b9b/191:100/w_1280,c_limit/amazon-a-logo.jpg"
+      companyName="Amazon" jobTitle="Senior UI/UX Designer" jobType="Full Time" level="Senior Level" salary="$120" location="Mumbai, India"
+      />
+      
     </div>
   )
 }
--- 03-card-project/src/assets/components/card.jsx
+++ 03-card-project/src/assets/components/card.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react'
-const Card = () => {
+const Card = (props) => {
   return (
     <>
-    <div className='parent'>
      <div className="card">
       <div className="top">
-        <img src="https://media.wired.com/photos/5a99ba72927dc94e67685b9b/191:100/w_1280,c_limit/amazon-a-logo.jpg" alt="" />
-        <button className="save-btn">Save<Bookmark className='save-icon'/></button>
+        <img src={props.src} alt="" />
+        <button className="save-btn"><Bookmark className='save-icon'/></button>
       </div>
-      <div className="center">
-        <h3>Amazon <span>5 days ago</span></h3>
-        <h2>Senior UI/UX Designer</h2>
+      <div className="center"> 
+        <h3>{props.companyName} <span>5 days ago</span></h3>
+        <h2>{props.jobTitle}</h2>
         <div>
-          <h4>Part Time</h4>
-          <h4>Senior lavel</h4>
+          <h4>{props.jobType}</h4>
+          <h4>{props.level}</h4>
         </div>
       </div>
       <div className="bottom">
          <div>
-          <h1>$120/hr</h1>
-          <p>Mumbai , India</p>
+          <h1>{props.salary}/hr</h1>
+          <p>{props.location}</p>
          </div>
          <button>Apply </button>
       </div>
      </div>
-    </div>
     </>
   )
 }
