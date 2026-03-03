2d2e286d4deab4cbf98d46e35209c7aaecaaa0a5
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-03 21:39:51 +0530

    chore: practice JSX CSS structure

---
+++ 04-css/src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/btn.css'
+const button = () => {
+  return (
+    <div>
+      <button className='btn'>click</button>
+    </div>
+  )
+}
+
+export default button
