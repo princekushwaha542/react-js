ec5c75138b5a594ff0fcec5b29337ddb6ce3a637
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-04 21:26:44 +0530

    feat: add 05-tailwindcss folder and setup tailwind with practice examples

---
+++ 05-tailwindcss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-400">Welcome to Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
