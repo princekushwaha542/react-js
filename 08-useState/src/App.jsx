5d205a069afcd94288d68088427b40fab1d28de4
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-15 08:42:31 +0530

    feat : implement counter project using usestate in react js

--- 08-useState/src/App.jsx
+++ 08-useState/src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, { useActionState, useState, useSyncExternalStore } from 'react'
 
 const App = () => {
-  let a = 20;
-  const changeA = () =>{
-    console.log(a);
-    a++;
-    console.log(a)
-  }
+  const [num , setNum ] = useState(0)
   return (
     <div>
-      <h1>vlaue of a {a} </h1>
-      <button onClick={changeA}>click</button>
+      <h1 className="p-20 m-5 bg-red-50 w-90 text-center text-4xl">{num}</h1>
+      <button onClick={ () =>{
+        setNum(num+1)
+      }} className='h-15 w-40 p-5 bg-yellow-500 ml-5 mr-5 '>increase</button>
+      <button onClick={() =>{
+        setNum(num-1)
+      }} className='h-15 w-40 p-5 bg-yellow-500 ml-5 mr-5 text-uppercase'>dicrease</button>
     </div>
   )
-}
-
+} 
 export default App
