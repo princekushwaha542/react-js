b897e389a0f70159b640e3649a793e522fa43996
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-19 21:33:11 +0530

    feat : implement notes project ui

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const submithandler = (elem) =>{
+    elem.preventDefault()
+    console.log(elem.target.textarea.value)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen bg-black text-white '>
+      <form onSubmit={(e) => {
+       submithandler(e)
+      }} className='flex p-10 flex-col gap-4 items-start w-1/2'>
+          <input 
+        type="text" name='title'
+        placeholder='Enter Notes Heading' 
+        className='px-5 py-2 border-2 rounded-xl w-full font-medium'
+        />
+         <textarea 
+        type="text" name='textarea'
+        placeholder='Write Details' 
+        className='px-5 py-2 h-40 border-2 rounded-xl w-full flex-start outline-none flex-row font-medium'
+        />
+        <button className='bg-white text-black px-5 py-3 rounded-xl w-full font-medium'>Add Note</button>
+      </form>
+    </div>
   )
 }
 
