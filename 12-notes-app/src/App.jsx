1fd58c80fc6f2e06fbbf10ad951ac0c06210cce6
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-21 21:38:17 +0530

    feat : implement notes app project ui

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -6,10 +6,11 @@ const App = () => {
     console.log(elem.target.textarea.value)
   }
   return (
-    <div className='h-screen bg-black text-white '>
+    <div className='h-screen bg-black text-white flex flex-wrap'>
       <form onSubmit={(e) => {
        submithandler(e)
-      }} className='flex p-10 flex-col gap-4 items-start w-1/2'>
+      }} className='flex p-10 flex-col gap-4 items-start lg:w-1/2 w-screen'>
+          <h1 className='text-4xl font-bold'>Add Notes</h1>
           <input 
         type="text" name='title'
         placeholder='Enter Notes Heading' 
@@ -22,6 +23,18 @@ const App = () => {
         />
         <button className='bg-white text-black px-5 py-3 rounded-xl w-full font-medium'>Add Note</button>
       </form>
+       <div className='bg-black  pt-10 pl-19 lg:w-1/2 w-screen lg:border-l-2'>
+          <h1 className='text-4xl font-bold'>Resent Notes</h1><br />
+          <div className='flex flex-wrap gap-5  h-full overflow-auto '>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+            <div className='h-62 w-50 rounded-2xl bg-white'></div>
+          
+          </div>
+       </div>
     </div>
   )
 }
