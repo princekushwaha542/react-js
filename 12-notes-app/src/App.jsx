2f7150b3f2e4373be1dccbf5eecd8df6963aa7bd
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-03-27 22:08:48 +0530

    feat : change resent notes ui to add notes png and fix task section

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -47,8 +47,11 @@ const App = () => {
       <div className='bg-black pt-10 pl-19 lg:w-1/2 w-screen lg:border-l-2'>
         <h1 className='text-4xl font-bold'>Recent Notes</h1><br />
         <div className='flex flex-wrap gap-5 h-full overflow-auto'>
-          {task.map((e,index) =>{
-            return <div className='h-62 w-50 rounded-2xl bg-white'></div>
+          {task.map((e,idx) =>{
+            return <div key={idx} className='h-62 w-50 rounded-2xl text-black bg-[url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVj4sD1SinFKKGDTgNYFrAQnBJX6jku1dAg&s")] bg-center bg-cover rotate-360'>
+              <h3 className='m-3 leading-tight font-bold uppercase text-xl'  >{e.title}</h3>
+              <p className='m-2 leading-tight text-gray-500 font-medium'>{e.details}</p>
+            </div>
           })}
          </div>
       </div>
