fb9a95d9858db68b68107513037a08e6fb0034cb
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-03 08:50:57 +0530

    feat : practice mounting in useeffect in react js

--- 15-useeffect/src/App.jsx
+++ 15-useeffect/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [num, setNum] = useState(0)
 
+  useEffect(() =>{
+    console.log('useEffect called')
+  })
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={()=>{
+        setNum(num+1)
+      }}>click</button>
+      <h1>{num}</h1>
+    </div>
   )
 }
 
