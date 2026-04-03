ed9ae41aae31a836a0113e75fca527ab8aa7a103
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-03 21:49:52 +0530

    feat : practice dipendancey in useeffect

--- 15-useeffect/src/App.jsx
+++ 15-useeffect/src/App.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(100)
 
   useEffect(() =>{
     console.log('useEffect called')
-  })
+  },[num])
   return (
     <div>
       <button onClick={()=>{
         setNum(num+1)
+        setNum2(num2+10)
+
       }}>click</button>
       <h1>{num}</h1>
     </div>
