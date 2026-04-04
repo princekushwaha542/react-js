6e6a519e02c3e81fbe9cbcc1f3140ff4675e4196
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-04 22:36:35 +0530

    feat : useeffect practice use of mini project

--- 15-useeffect/src/App.jsx
+++ 15-useeffect/src/App.jsx
@@ -1,7 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import Hooks from './hooks'
 const App = () => {
-  <Hooks/>
+    
+    const [a, setA] = useState(0)
+    const [b, setB] = useState(10)
+
+    function clickAbtn(){
+        console.log('button A clicked')
+        setA(a+1)
+      
+    }
+    function clickBbtn() {
+        console.log('button B clicked')
+        setB(b+10)
+    }
+
+    useEffect(() => {
+        console.log(a)
+    },[a])
+
+    useEffect ( () => {
+        console.log(b)
+    } ,[b] )
+  return (
+    <div>
+      <h2>{a}</h2>
+      <h2>{b}</h2>
+      <button onClick={clickAbtn}>button A</button>
+      <button onClick={clickBbtn}>button B</button>
+    </div>
+  )
 }
+
 
 export default App
--- 15-useeffect/src/hooks.jsx
+++ 15-useeffect/src/hooks.jsx
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const hooks = () => {
-    
-    const [a, setA] = useState(0)
-    const [b, setB] = useState(10)
-
-    function clickAbtn(){
-        console.log('button A clicked')
-
-    }
-    function clickBbtn() {
-        console.log('button B clicked')
-    }
-
-    useEffect(() => {
-        clickAbtn()
-    },[a])
-
-    useEffect ( () => {
-        clickBbtn()
-
-    } ,[b] )
   return (
     <div>
-      <button>button A</button>
-      <button>button B</button>
+      
     </div>
   )
 }
