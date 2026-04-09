43cf38064092fed083a7ff71df7dbb34b0613ca9
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-09 22:09:17 +0530

    feat : fix gallery project

--- 16-gallery-project/src/App.jsx
+++ 16-gallery-project/src/App.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-const App = () => {
-  const [ userData , setUserData ] = useState([])
 
-  const GetData = async () =>{
-    const response = await axios.get('https://picsum.photos/v2/list')
-      setUserData(response.data)     
+const App = () => {
+  const [userData, setUserData] = useState([])
+
+  const GetData = async () => {
+    try {
+      const response = await axios.get('https://picsum.photos/v2/list')
+      setUserData(response.data)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  let printuserData = 'No user available';
-  if(userData.length > 0){
-    printuserData = userData.map((user,idx) => {
-      return <div>
-        <img className='h-40' src={user.download_url} alt="" />
-      </div>
-    })
-  }
+
   return (
-    <div className='h-screen bg-black text-white pt-4'>
-       <button 
-       onClick={GetData}
-       className='bg-green-300 p-4 m-4 active:scale-95 text-2xl text-black rounded-2xl'
-       >
+    <div className='min-h-screen bg-black text-white p-4'>
+      
+      <button 
+        onClick={GetData}
+        className='bg-green-300 p-4 m-4 active:scale-95 text-2xl text-black rounded-2xl'
+      >
         Get Data
-       </button>
-       <img src={userData} alt="image" />
-       <img src={userData} alt="image" />
-       <h1 className='flex flex-wrap gap-3 overflow-hidden'>{printuserData}</h1>
+      </button>
+
+      <div className='flex flex-wrap gap-4'>
+        {userData.length > 0 ? (
+          userData.map((user, idx) => (
+            <div key={idx}>
+              <img 
+                className='h-40 rounded-lg' 
+                src={user.download_url} 
+                alt="img" 
+              />
+            </div>
+          ))
+        ) : (
+          <h2>No data available</h2>
+        )}
+      </div>
+
     </div>
   )
 }
