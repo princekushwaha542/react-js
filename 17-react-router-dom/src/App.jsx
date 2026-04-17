502d3421f424aa5a0151b00e5575476a91ce26e6
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-17 07:24:17 +0530

    feat : practice react router dom to creat a multipul section on single page application

--- 17-react-router-dom/src/App.jsx
+++ 17-react-router-dom/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'  // ✔️ correct
 
 const App = () => {
   return (
     <div>
-      this is react router dom
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
