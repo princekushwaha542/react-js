3fa4fda8c4914d380f3b7783827c5e445e7199f2
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-18 22:22:37 +0530

    feat : fix react router dom project

--- 17-react-router-dom/src/App.jsx
+++ 17-react-router-dom/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'  // ✔️ correct
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <div>
+      <div className='flex justify-center align-center gap-5 text-white bg-black p-4'>
+        <h3><Link to="/">Home</Link></h3>
+        <h3><Link to="/about">About</Link></h3>
+        <h3><Link to="/contact">Contact</Link></h3>
+      </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
