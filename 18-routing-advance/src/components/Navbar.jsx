74d5ed3f2f507a035ceafcf237b024bb7db2d026
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-04-27 23:09:38 +0530

    feat : fix to practice Link in react router dom

--- 18-routing-advance/src/components/Navbar.jsx
+++ 18-routing-advance/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='h-15 w-full bg-gray-500 flex items-center justify-between pr-20 '>
       <h1 className='text-xl font-bold w-110 ml-20'>Shyriansh</h1>
-      <div className='flex gap-10'>
-      <a href="/home">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-      <a href="/services">Services</a>
+       <div className='flex gap-10'>
+        <Link className='text-lg font-medium' to="/home">Home</Link>
+        <Link className='text-lg font-medium' to="/about">About</Link>
+        <Link className='text-lg font-medium' to="/contact">Contact</Link>
+        <Link className='text-lg font-medium' to="/services">Services</Link>
       </div>
     </div>
   )
