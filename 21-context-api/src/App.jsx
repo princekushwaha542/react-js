b016792badf567b160d57e465681ca84599ad0ff
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-11 23:18:53 +0530

    fix  : use data to handel data form child file

--- 21-context-api/src/App.jsx
+++ 21-context-api/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './components/Navbar'
 import Nav2 from './components/Nav2'
+import {PostContextData} from './ThmeContext'
 const App = () => {
+  const data = useContext(PostContextData)
+  console.log(data)
   return (
+  
     <div className='flex bg-gray-300 w-full h-10 justify-between items-center p-6'>
       <Navbar></Navbar>
       <Nav2></Nav2>
+      
     </div>
   )
 }
