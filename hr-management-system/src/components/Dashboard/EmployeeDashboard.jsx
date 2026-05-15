172daf04806b7162f304f292efb7bb9665eee6af
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-15 23:52:47 +0530

    feat : add task section

--- hr-management-system/src/components/Dashboard/EmployeeDashboard.jsx
+++ hr-management-system/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../other/header'
+import TaskListNumbers from '../other/TaskListNumbers'
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1c1c1c] h-screen'>
       <Header/>
+      <TaskListNumbers/>
     </div>
   )
 }
--- hr-management-system/src/components/other/TaskListNumbers.jsx
+++ hr-management-system/src/components/other/TaskListNumbers.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const TaskListNumbers = () => {
   return (
-    <div>
-      
+    <div className='flex screen gap-5 justify-between mt-10'>
+      <div className='rounded-xl p-4 w-[45%] py-6 px-9 bg-amber-800 text-white'>
+        <h2 className='text-3xl font-semibold'>1</h2>
+        <p className='text-sm mt-2'>New task</p>
+      </div>
+      <div className='rounded-xl p-4 w-[45%] py-6 px-9 bg-amber-800 text-white'>
+        <h2 className='text-3xl font-semibold'>1</h2>
+        <p className='text-sm mt-2'>New task</p>
+      </div><div className='rounded-xl p-4 w-[45%] py-6 px-9 bg-amber-800 text-white'>
+        <h2 className='text-3xl font-semibold'>1</h2>
+        <p className='text-sm mt-2'>New task</p>
+      </div>
     </div>
   )
 }
