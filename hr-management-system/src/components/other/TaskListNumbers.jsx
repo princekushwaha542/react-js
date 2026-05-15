fcb5f825f3fc67f8199e9cb7c5c28da463dcedd4
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-15 23:38:28 +0530

    feat : add TaskListNumber in hr-management-system project

---
+++ hr-management-system/src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskListNumbers = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TaskListNumbers
--- hr-management-system/src/components/other/header.jsx
+++ hr-management-system/src/components/other/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='text-white flex items-center justify-between'>
         <h2 className='text-2xl'>hello <br /><span className='text-3xl font-semibold'>Prince 👋</span></h2>
-        <button>Log out</button>
+        <button className='bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded'>Log out</button>
     </div>
   )
 }
