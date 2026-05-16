81799f6240584263d0b1f9f66c5341b948bd56b1
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-16 23:36:36 +0530

    fix : tasklist dashboard UI

--- hr-management-system/src/components/TaskList/TaskList.jsx
+++ hr-management-system/src/components/TaskList/TaskList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='flex h-[55%] w-[100%] gap-4 justify-start items-center overflow-x-auto bg-amber-900 rounded-xl mt-10 mr-10 flex-nowrap p-4'> 
+    <div id='tasklist' className='flex h-[55%] w-[100%] gap-4 justify-start items-center overflow-x-auto bg-amber-900 rounded-xl mt-10 mr-10 flex-nowrap p-4'> 
       <div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
       </div>
       <div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
