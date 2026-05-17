82bd1498af899d1dc33e9d435e62a8a3930f0293
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-17 23:00:46 +0530

    fix : tasklist dashboard

--- hr-management-system/src/components/TaskList/TaskList.jsx
+++ hr-management-system/src/components/TaskList/TaskList.jsx
@@ -5,14 +5,7 @@ const TaskList = () => {
     <div id='tasklist' className='flex h-[55%] w-[100%] gap-4 justify-start items-center overflow-x-auto bg-amber-900 rounded-xl mt-10 mr-10 flex-nowrap p-4'> 
       <div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
       </div>
-      <div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div><div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
-      </div>
+      
     </div>
   )
 }
