b015899572b36c2cee3343eefe7a04da32b7cd35
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-18 21:33:13 +0530

    feat : add task and task components

--- hr-management-system/src/components/TaskList/TaskList.jsx
+++ hr-management-system/src/components/TaskList/TaskList.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div id='tasklist' className='flex h-[55%] w-[100%] gap-4 justify-start items-center overflow-x-auto bg-amber-900 rounded-xl mt-10 mr-10 flex-nowrap p-4'> 
-      <div className='flex-shrink-0 h-full w-[300px] bg-gray-500'>
+    <div id='tasklist' className='select-none flex px-3 py-3 h-[55%] w-[100%] gap-4 justify-start items-center overflow-x-auto bg-amber-900 rounded-xl mt-10 mr-10 flex-nowrap p-4'> 
+      <div className='flex-shrink-0 h-full w-[300px] bg-red-400'>
+        <div className='flex justify-between items-center px-3 py-2'>
+          <h3 className='px-3 py-1 bg-amber-700 text-white rounded'>High</h3>
+          <h4 className='text-white text-sm'>18  May 2026</h4>
+
+        </div>
+        <h2 className='text-xl text-gray-800 mt-5 m-3'>hr-management project</h2>
+        <p className='text-white text-sm mt-4 m-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur nobis nam eos ut, quod voluptatibus.</p>
       </div>
       
     </div>
