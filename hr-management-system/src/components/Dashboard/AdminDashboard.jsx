a7a7f93a12e89a7e2d860fe28086e792231d410b
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-20 22:21:06 +0530

    fix : update AdminDashboard ui

--- hr-management-system/src/components/Dashboard/AdminDashboard.jsx
+++ hr-management-system/src/components/Dashboard/AdminDashboard.jsx
@@ -6,27 +6,27 @@ const AdminDashboard = () => {
       <Header />
        <form className=' bg-red-50 pt-20 pb-25' >
         <div className='flex items-center justify-around w-screen'>
-          <div>
-        <h2>Task Title</h2>
-        <input className='w-30 h-6' type="text" placeholder='input title' />
-       </div>
+          <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
+          <h3>Task Title</h3>
+          <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="text" placeholder='input title' />
+        </div>
         
-        <div>
+        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
           <h3>Date</h3>
-        <input className='w-30 h-6' type="date" />
+        <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="date" />
         </div>
-        <div>
+        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
           <h3>Assign to </h3>
-          <input className='w-30 h-6' type="text" placeholder='employee name' />
+          <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="text" placeholder='employee name' />
         </div>
-        <div>
+        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
           <h3>Category</h3>
-          <input className='w-30 h-6' type="text" placeholder='Design , Development etc.' />
+          <input className='w-52 h-6 bg-white border border-gray-800 pl-3 p-3' type="text" placeholder='Design , Development etc.' />
         </div>
         </div>
-        <div className=' flex w-full block mt-20 items-center justify-center'>
+        <div className=' flex w-full block gap-5 mt-20 items-center justify-start pl-100'>
           <h3>Task Description</h3>
-        <textarea placeholder='input description' />
+        <textarea className='w-70 h-14 bg-white border border-gray-800 pl-3 p-3' placeholder='input description' />
         </div>
         <button className='bg-red-500 p-2 rounded-2xl flex w-40 block mt-40 ml-150 items-center justify-center' type='submit'>Create Task</button>
        </form>
