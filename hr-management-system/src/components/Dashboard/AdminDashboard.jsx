e285c9fdab81b9666eefac06bf4b91be74af36ec
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-20 23:01:51 +0530

    fix : update AdminDashboard UI

--- hr-management-system/src/components/Dashboard/AdminDashboard.jsx
+++ hr-management-system/src/components/Dashboard/AdminDashboard.jsx
@@ -4,31 +4,33 @@ const AdminDashboard = () => {
   return (
     <div>
       <Header />
-       <form className=' bg-red-50 pt-20 pb-25' >
-        <div className='flex items-center justify-around w-screen'>
-          <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
+       <form className='  pt-20 pb-25 flex  justify-between p-10 pr-70 bg-stone-200' >
+        <div className='flex flex-col  items-start justify-start '>
+          <div className='pt-5 pl-5 flexitems-center justify-center rounded-2xl'>
           <h3>Task Title</h3>
-          <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="text" placeholder='input title' />
+          <input className='w-80 h-8 rounded-l  mt-1 bg-stone-100 border border-white pl-5 p-3' type="text" placeholder='Make a UI design ' />
         </div>
         
-        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
+        <div className='pt-5 pl-5 flexitems-center justify-center rounded-2xl'>
           <h3>Date</h3>
-        <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="date" />
+        <input className='w-80 h-8 rounded-l  mt-1 bg-stone-100 border border-white pl-5 p-3' type="date" />
         </div>
-        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
+        <div className='pt-5 pl-5 flex flex-col items-start rounded-2xl'>
           <h3>Assign to </h3>
-          <input className='w-52 h-6 bg-white border border-gray-800 pl-5 p-3' type="text" placeholder='employee name' />
+          <input className='w-80 h-8 rounded-l  mt-1 bg-stone-100 border border-white pl-5 p-3' type="text" placeholder='Employee name' />
         </div>
-        <div className='p-5 flex flex-col bg-red-200 items-center justify-center rounded-2xl'>
+        <div className='pt-5 pl-5 flex flex-col items-start justify-center rounded-2xl'>
           <h3>Category</h3>
-          <input className='w-52 h-6 bg-white border border-gray-800 pl-3 p-3' type="text" placeholder='Design , Development etc.' />
+          <input className='w-80 h-8 rounded-l  mt-1 bg-stone-100 border border-white pl-5 ' type="text" placeholder='Design , Development etc.' />
         </div>
         </div>
-        <div className=' flex w-full block gap-5 mt-20 items-center justify-start pl-100'>
+        <div>
+          <div className=' ml-90 mt-5'>
           <h3>Task Description</h3>
-        <textarea className='w-70 h-14 bg-white border border-gray-800 pl-3 p-3' placeholder='input description' />
+        <textarea className='w-110 h-60  border border-gray-800 pl-3 p-3' placeholder='Input description' />
         </div>
-        <button className='bg-red-500 p-2 rounded-2xl flex w-40 block mt-40 ml-150 items-center justify-center' type='submit'>Create Task</button>
+        <button className='ml-90 w-110 h-10 bg-green-400  text-white rounded-2xl mt-5' type='submit'>Create Task</button>
+        </div>
        </form>
     </div>
   )
