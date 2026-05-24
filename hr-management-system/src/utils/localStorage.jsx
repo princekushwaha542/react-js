b8e8d6937cd9a1786c77a8cfe0750ea71437faa3
Prince Kushwaha <kushwahaprince43668@gmail.com>
2026-05-24 23:51:58 +0530

    feat : add employees data and store in  local storage

--- hr-management-system/src/utils/localStorage.jsx
+++ hr-management-system/src/utils/localStorage.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
 
 const localStorage = () => {
+  {
+  const data "employees": [
+    {
+      "id": 1,
+      "email": "john@example.com",
+      "password": "123"
+    },
+    {
+      "id": 2,
+      "email": "emma@example.com",
+      "password": "123"
+    },
+    {
+      "id": 3,
+      "email": "michael@example.com",
+      "password": "123"
+    },
+    {
+      "id": 4,
+      "email": "sophia@example.com",
+      "password": "123"
+    },
+    {
+      "id": 5,
+      "email": "david@example.com",
+      "password": "123"
+    }
+  ],
+  "admin": {
+    "id": 101,
+    "email": "admin@example.com",
+    "password": "123"
+  }
+}
   return (
     <div>
       
